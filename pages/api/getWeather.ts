9a7a62c5e3392f61b3c274e7ad2f23a123b2c05b
katharina jäger <jaeger.kt@gmail.com>
2022-05-02 12:35:15 +0200

    Comment out code to find error in production

--- pages/api/getWeather.ts
+++ pages/api/getWeather.ts
@@ -20,63 +20,67 @@ export default async function getWeather(
         response.json({ error: 'Failed to fetch the workouts' });
         return;
       }
-      // get the single workout that we need the forecast for
-      const requestedWorkoutInfo = workoutBody.hits.hits.find(
-        (item) => item._source.activityDate.objectId === request.body.objectId,
-      );
-      if (!requestedWorkoutInfo) {
-        response.status(404).json({ error: "Can't find your workout" });
-        return;
-      }
-      // get the date of the workout
-      const workoutDate =
-        requestedWorkoutInfo._source.activityDate.start.iso.slice(0, 10);
-
-      // get the city of the workout
-      const workoutLocationLat = requestedWorkoutInfo._source.location.latitude;
-      const workoutLocationLon =
-        requestedWorkoutInfo._source.location.longitude;
-
-      // for debugging netlify
-      if (!process.env.API_KEY) {
-        response
-          .status(502)
-          .json({ error: 'Problem using the environment variables' });
-        return;
-      }
-      // get the weather forecast for the specific city
-      const forecastResponse = await fetch(
-        `https://api.weatherbit.io/v2.0/forecast/daily?&lat=${workoutLocationLat}&lon=${workoutLocationLon}&key=${process.env.API_KEY}`,
-      );
-      const forecastBody: { data: Forecast[] } | undefined =
-        await forecastResponse.json();
-
-      if (!forecastBody) {
-        response
-          .status(404)
-          .json({ error: 'Failed to fetch the weather forecast' });
-        return;
-      }
-
-      // use only the forecast for the day of the workout
-      const dateForecast = forecastBody.data.filter(
-        (weather) =>
-          weather.valid_date ===
-          // BUT these dates are in the past, so add one year to the date
-          (
-            Number(workoutDate.slice(0, 4)) +
-            1 +
-            workoutDate.slice(4)
-          ).toString(),
-      );
-      // send the activityDate and the forecast in the response body
       response.status(200).json({
-        error: 'Testing if this is returned',
-        // weather: dateForecast[0],
-        // activityDate: requestedWorkoutInfo._source.activityDate,
-        // city: requestedWorkoutInfo._source.city,
+        error: 'Testing if this is returned 1',
       });
       return;
+      // // get the single workout that we need the forecast for
+      // const requestedWorkoutInfo = workoutBody.hits.hits.find(
+      //   (item) => item._source.activityDate.objectId === request.body.objectId,
+      // );
+      // if (!requestedWorkoutInfo) {
+      //   response.status(404).json({ error: "Can't find your workout" });
+      //   return;
+      // }
+      // // get the date of the workout
+      // const workoutDate =
+      //   requestedWorkoutInfo._source.activityDate.start.iso.slice(0, 10);
+
+      // // get the city of the workout
+      // const workoutLocationLat = requestedWorkoutInfo._source.location.latitude;
+      // const workoutLocationLon =
+      //   requestedWorkoutInfo._source.location.longitude;
+
+      // // for debugging netlify
+      // if (!process.env.API_KEY) {
+      //   response
+      //     .status(502)
+      //     .json({ error: 'Problem using the environment variables' });
+      //   return;
+      // }
+      // // get the weather forecast for the specific city
+      // const forecastResponse = await fetch(
+      //   `https://api.weatherbit.io/v2.0/forecast/daily?&lat=${workoutLocationLat}&lon=${workoutLocationLon}&key=${process.env.API_KEY}`,
+      // );
+      // const forecastBody: { data: Forecast[] } | undefined =
+      //   await forecastResponse.json();
+
+      // if (!forecastBody) {
+      //   response
+      //     .status(404)
+      //     .json({ error: 'Failed to fetch the weather forecast' });
+      //   return;
+      // }
+
+      // // use only the forecast for the day of the workout
+      // const dateForecast = forecastBody.data.filter(
+      //   (weather) =>
+      //     weather.valid_date ===
+      //     // BUT these dates are in the past, so add one year to the date
+      //     (
+      //       Number(workoutDate.slice(0, 4)) +
+      //       1 +
+      //       workoutDate.slice(4)
+      //     ).toString(),
+      // );
+      // // send the activityDate and the forecast in the response body
+      // response.status(200).json({
+      //   error: 'Testing if this is returned',
+      //   // weather: dateForecast[0],
+      //   // activityDate: requestedWorkoutInfo._source.activityDate,
+      //   // city: requestedWorkoutInfo._source.city,
+      // });
+      // return;
     } catch {
       response
         .status(404)
